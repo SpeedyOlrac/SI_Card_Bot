51fbc4d973b1552701c3953b9d0b83a00b5d87ab
SpeedyOlrac <Speedyolrac@yahoo.com>
2020-08-12 14:10:04 -0400

    Cross module test

--- Names.js
+++ Names.js
@@ -1,7 +1,7 @@
 
 
-module.export = { 
-    var: spirits = [
+ 
+var spirits = [
     'A Spread of Rampant Green',
     'Bringer of Dreams and Nightmares',
     'Downpour Drenches the World',
@@ -26,7 +26,68 @@ module.export = {
     'Vengeance as a Burning Plague',
     'Vital Strength of the Earth',
     'Volcano Looming High'
- ] }
+ ];
+
+
+/*
+https://imgur.com/nlpGjjH
+https://imgur.com/5iTl68l
+https://imgur.com/XOC05vn
+https://imgur.com/wgs13CF
+https://imgur.com/KZwtr5W
+https://imgur.com/5xnsvD8
+https://imgur.com/RqlC8ZP
+https://imgur.com/PztVF5L
+https://imgur.com/4hnimN9
+https://imgur.com/eZbTaHA
+https://imgur.com/9hiaoWd
+https://imgur.com/pCuo4XJ
+https://imgur.com/2UujxIG
+https://imgur.com/MQDsVg9
+https://imgur.com/sE5Iasm
+https://imgur.com/oyk2ZwH
+https://imgur.com/uxw6S5D
+https://imgur.com/H1Xrzxw
+https://imgur.com/IoL5BHz
+https://imgur.com/BB48p9Q
+https://imgur.com/FmVImnv
+https://imgur.com/cE7YZ3L
+https://imgur.com/PgT7Kyj
+https://imgur.com/uv0mMRV
+https://imgur.com/pmdjfxu
+https://imgur.com/wGPod8z
+https://imgur.com/RZh8Y8g
+https://imgur.com/rrLWF5o
+https://imgur.com/XIG39qe
+https://imgur.com/UVEIJfA
+https://imgur.com/OmSNTeL
+https://imgur.com/WLcU0w9
+https://imgur.com/yXE6oHw
+https://imgur.com/ZKAblG3
+https://imgur.com/owZAHJY
+https://imgur.com/XvJYfqH
+https://imgur.com/i6YLeWv
+https://imgur.com/AJjfgOS
+https://imgur.com/PBwq5KE
+https://imgur.com/Y8lVu5J
+https://imgur.com/A4FRUV5
+https://imgur.com/VUOjO0v
+https://imgur.com/CShexIA
+https://imgur.com/7z7V6Ol
+https://imgur.com/g0S3eN8
+https://imgur.com/UxZyTv2
+https://imgur.com/iK9NTsz
+https://imgur.com/JtdpxCr
+
+
+
+*/
+
+
+
+
+
+exports.spirits = spirits;
 
 
 
