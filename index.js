dcf219e1a8c843c534bc71c91f3fddb2ddb33f8a
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2020-08-19 09:29:33 -0400

    no more vart

--- index.js
+++ index.js
@@ -203,10 +203,10 @@ bot.on('message', async msg =>{
         case'random':
             if(args[1]){
               let answer = Picking(args[1], Names.spirits, args[2], args[3]);
-              var botMessage1 = msg.channel.send(answer[0]);
+              botMessage1 = msg.channel.send(answer[0]);
               if(answer[1]){
                 
-                var botMessage2 = await msg.channel.send(answer[1]);
+                botMessage2 = await msg.channel.send(answer[1]);
                 await delay(15);
                 msg.delete();
                 botMessage1.delete();
