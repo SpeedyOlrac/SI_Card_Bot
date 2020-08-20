a58bb33eaf2204589c74665a7bb4d1e0a4a6133d
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2020-08-19 23:56:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ bot.on('message', msg =>{
     if(!msg.content.startsWith(PREFIX) ){ return;}
 
     const args = msg.content.slice(PREFIX.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+	//const command = args.shift().toLowerCase();
     
     /*if (bot.commands.has(command)) {
 
