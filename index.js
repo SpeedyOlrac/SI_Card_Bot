b94b18ba51183f02385a88a33927255e19e3a7ce
Marc-Andre Paris-Cloutier <marc-andrepc@hotmail.com>
2020-08-21 08:51:22 -0400

    Wrapping the whole message handling code in a try catch with notification.

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const fs = require('fs');
 
 bot.commands = new Discord.Collection();
 
-//test push
-
 /*
     Author: Carlo I Gonzalez "SpeedyOlrac"
     Desciption: THis bot is made to help spirit island card and spirit panel look ups.
@@ -35,192 +33,198 @@ var roleID = "<@&743228206806728766>";
 var messageID = '743535158795173898';
 var emojiID = "<:FlagBlank:742199330018164776>";
 
-bot.on('ready', () => {
+const GudradainID = '<@151904548959682561>';
+const SpeedyOlracID = '<@176329826641117186>';
 
+bot.on('ready', () => {
     console.log('This bot is online');
 });
 
 
 bot.on('message', msg =>{
-	
-    //Checks if using the Correct Prefix, might have to change to a - oneday
+	try{
+    processMessage(msg);
+  } catch(error) {
+    console.error(error);
+    msg.channel.send('Unexpected error. ' + SpeedyOlracID + ' ' + GudradainID);
+  }
+});
 
-    if(!msg.content.startsWith(PREFIX) ){ return;}
+function processMessage(msg)
+{
+  //Checks if using the Correct Prefix, might have to change to a - oneday
 
-    const args = msg.content.slice(PREFIX.length).trim().split(/ +/);
-    try{
+  if(!msg.content.startsWith(PREFIX) ){ return;}
+
+  const args = msg.content.slice(PREFIX.length).trim().split(/ +/);
+
+  //const command = args.shift().toLowerCase();
+
+  /*if (bot.commands.has(command)) {
+
+      try {
+        bot.commands.get(command).execute(message, args);
+          } catch (error) {
+          console.error(error);
+          message.reply('there was an error trying to execute that command!');
+      }
+  }
+  */
+  switch(args[0]){
+
+    case 'error':
       var test = messageDontExist.split(',');
-    } catch(error) {
-      console.log(error);
-      msg.channel.send('Unexpected error: <@151904548959682561>');
-    }
-    
-	//const command = args.shift().toLowerCase();
-    
-    /*if (bot.commands.has(command)) {
+      break;
 
-        try {
-	        bot.commands.get(command).execute(message, args);
-            } catch (error) {
-            console.error(error);
-            message.reply('there was an error trying to execute that command!');
+    //Help, list of options bot can preform
+    case 'help':
+        msg.channel.send(Names.help);
+        break;
+
+    //Search the Sick card catalog
+    case 'search':
+        if(args[1] == 'help'){
+            msg.channel.send(Names.sHelp);
+            break;
         }
+        args.shift();
+        var site_name = "https://sick.oberien.de/?query=" + args.toString().replace(/,/g, "%20");
+        if(UrlExists(site_name)){
+            msg.channel.send(site_name);
+        }
+        else{
+            msg.channel.send("Incorrect Syntax, try !search help");
+        }
+        break;
+    
+    //Looks up the SicK website using inputed name. Correct name returns a website
+    // Returns an error is a 404 page is returned
+    case 'card' :
+    case 'power':
+        sendCardLink(msg, args, ImageNames.power, "https://sick.oberien.de/imgs/powers/");
+        break;
+        
+    //Looks up the SicK website using inputed name.Correct name returns a website
+    // Returns an error is a 404 page is returned
+    case 'event':
+        sendCardLink(msg, args, ImageNames.event, "https://sick.oberien.de/imgs/events/");
+        break;
+    
+    //Looks up the SicK website using inputed name. Correct name returns a website
+    //Returns an error is a 404 page is returned
+    case 'fear':
+        sendCardLink(msg, args, ImageNames.fear, "https://sick.oberien.de/imgs/fears/");
+        break;
+        
+    //Looks up the SicK website using inputed name.Correct name returns a website
+    // Returns an error is a 404 page is returned
+    case 'minor':
+        sendCardLink(msg, args, ImageNames.minor, "https://sick.oberien.de/imgs/powers/");
+        break;
+
+    //Looks up the SicK website using inputed name.Correct name returns a website
+    // Returns an error is a 404 page is returned
+    case 'major':
+        sendCardLink(msg, args, ImageNames.major, "https://sick.oberien.de/imgs/powers/");
+        break;
+    
+    //Looks up the SicK website using inputed name.Correct name returns a website
+    // Returns an error is a 404 page is returned
+    case 'unique':
+        sendCardLink(msg, args, ImageNames.unique, "https://sick.oberien.de/imgs/powers/");
+        break;
+
+    //Look up on the faq, will accpect anything. Returns website search. 
+    case 'faqs':
+        if (!args[1]){
+            msg.channel.send("https://querki.net/u/darker/spirit-island-faq/#!.3y28a87");
+            break;
+        }
+        var site_name = "https://querki.net/u/darker/spirit-island-faq/#!Search-Results?query=\"\"" + 
+                cleanInput(args).replace(/,/g, '%20') + "%22%22" ;
+        console.log(site_name);
+        if(UrlExists(site_name)){
+            msg.channel.send(site_name);
+        }
+        else{
+            msg.channel.send("Incorrect name, try again");
+        }
+        break;       
+
+    //Selects a random spirit or adversary. Return a name and a emote of the selection.
+    case'random':
+        if(args[1]){
+          let answer = Picking(args[1], Names.spirits, args[2], args[3]);
+          botMessage1 = msg.channel.send(answer[0]);
+          if(answer[1]){
+            
+            botMessage2 = msg.channel.send(answer[1]);
+            /*
+            
+            await delay(15);
+            msg.delete();
+            botMessage1.delete();
+            botMessage2.delete();
+            */
+          }
+        }
+        else{
+            msg.channel.send("Do you want a random [spirit] or [adversary]?")
+        }
+        break;
+
+    //TODO, find workable website with images
+    //Looks for a spirit using limited word search, one part of the name has to be correct.
+    //Returns a both Spirit Panel unless user specifies front or back.
+
+    case 'spirit':
+        var list = args;
+        console.log(list);
+        var target = "Sorry could not find the spirit you where looking for.";
+        var found = false;
+
+        outer_loop:
+        console.log(list.length + " " + Names.spirits.length);
+        for(var l = 1; l < list.length; l++){
+            for( var s = 0; s < Names.spirits.length; s++){
+                var name = Names.spirits[s].split(" ");
+                for(var n = 0; n < name.length; n++){
+
+                    console.log(list[l] + " " + name);
+                    if (!found && list[l].length > 3 && name[n].toLowerCase() == list[l].toLowerCase()){                        
+                        target = s;
+                        found = true;
+                        break ;//outer_loop;
+                     } 
+                }
+            }               
+        }
+
+        if(found){
+            if(args[1].toLowerCase() != 'back'){
+                msg.channel.send(Names.panel[target][0]);
+            }
+            if(args[1].toLowerCase() != 'front' ){
+                msg.channel.send(Names.panel[target][1]); 
+            }
+        }
+        else{
+            msg.channel.send(target);
+        }
+        break;
+    
+
+    case 'rolebot':
+        if (args[1] == 'number'){
+            let LFGRole = args[1];
+            return;
+        }
+
+        message.member.roles.add(LFGRole);
+
+        break;
     }
-*/
-    switch(args[0]){
-
-        case 'error':
-          msg.channel.send('Unexpected error: <@151904548959682561>');
-          break;
-
-        //Help, list of options bot can preform
-        case 'help':
-            msg.channel.send(Names.help);
-            break;
-
-        //Search the Sick card catalog
-        case 'search':
-            if(args[1] == 'help'){
-                msg.channel.send(Names.sHelp);
-                break;
-            }
-            args.shift();
-            var site_name = "https://sick.oberien.de/?query=" + args.toString().replace(/,/g, "%20");
-            if(UrlExists(site_name)){
-                msg.channel.send(site_name);
-            }
-            else{
-                msg.channel.send("Incorrect Syntax, try !search help");
-            }
-            break;
-        
-        //Looks up the SicK website using inputed name. Correct name returns a website
-        // Returns an error is a 404 page is returned
-        case 'card' :
-        case 'power':
-            sendCardLink(msg, args, ImageNames.power, "https://sick.oberien.de/imgs/powers/");
-            break;
-            
-        //Looks up the SicK website using inputed name.Correct name returns a website
-        // Returns an error is a 404 page is returned
-        case 'event':
-            sendCardLink(msg, args, ImageNames.event, "https://sick.oberien.de/imgs/events/");
-            break;
-        
-        //Looks up the SicK website using inputed name. Correct name returns a website
-        //Returns an error is a 404 page is returned
-        case 'fear':
-            sendCardLink(msg, args, ImageNames.fear, "https://sick.oberien.de/imgs/fears/");
-            break;
-            
-        //Looks up the SicK website using inputed name.Correct name returns a website
-        // Returns an error is a 404 page is returned
-        case 'minor':
-            sendCardLink(msg, args, ImageNames.minor, "https://sick.oberien.de/imgs/powers/");
-            break;
-
-        //Looks up the SicK website using inputed name.Correct name returns a website
-        // Returns an error is a 404 page is returned
-        case 'major':
-            sendCardLink(msg, args, ImageNames.major, "https://sick.oberien.de/imgs/powers/");
-            break;
-        
-        //Looks up the SicK website using inputed name.Correct name returns a website
-        // Returns an error is a 404 page is returned
-        case 'unique':
-            sendCardLink(msg, args, ImageNames.unique, "https://sick.oberien.de/imgs/powers/");
-            break;
-
-        //Look up on the faq, will accpect anything. Returns website search. 
-        case 'faqs':
-            if (!args[1]){
-                msg.channel.send("https://querki.net/u/darker/spirit-island-faq/#!.3y28a87");
-                break;
-            }
-            var site_name = "https://querki.net/u/darker/spirit-island-faq/#!Search-Results?query=\"\"" + 
-                    cleanInput(args).replace(/,/g, '%20') + "%22%22" ;
-            console.log(site_name);
-            if(UrlExists(site_name)){
-                msg.channel.send(site_name);
-            }
-            else{
-                msg.channel.send("Incorrect name, try again");
-            }
-            break;       
-
-        //Selects a random spirit or adversary. Return a name and a emote of the selection.
-        case'random':
-            if(args[1]){
-              let answer = Picking(args[1], Names.spirits, args[2], args[3]);
-              botMessage1 = msg.channel.send(answer[0]);
-              if(answer[1]){
-                
-                botMessage2 = msg.channel.send(answer[1]);
-                /*
-                
-                await delay(15);
-                msg.delete();
-                botMessage1.delete();
-                botMessage2.delete();
-                */
-              }
-            }
-            else{
-                msg.channel.send("Do you want a random [spirit] or [adversary]?")
-            }
-            break;
-
-        //TODO, find workable website with images
-        //Looks for a spirit using limited word search, one part of the name has to be correct.
-        //Returns a both Spirit Panel unless user specifies front or back.
-
-        case 'spirit':
-            var list = args;
-            console.log(list);
-            var target = "Sorry could not find the spirit you where looking for.";
-            var found = false;
-
-            outer_loop:
-            console.log(list.length + " " + Names.spirits.length);
-            for(var l = 1; l < list.length; l++){
-                for( var s = 0; s < Names.spirits.length; s++){
-                    var name = Names.spirits[s].split(" ");
-                    for(var n = 0; n < name.length; n++){
-
-                        console.log(list[l] + " " + name);
-                        if (!found && list[l].length > 3 && name[n].toLowerCase() == list[l].toLowerCase()){                        
-                            target = s;
-                            found = true;
-                            break ;//outer_loop;
-                         } 
-                    }
-                }               
-            }
-
-            if(found){
-                if(args[1].toLowerCase() != 'back'){
-                    msg.channel.send(Names.panel[target][0]);
-                }
-                if(args[1].toLowerCase() != 'front' ){
-                    msg.channel.send(Names.panel[target][1]); 
-                }
-            }
-            else{
-                msg.channel.send(target);
-            }
-            break;
-        
-
-        case 'rolebot':
-            if (args[1] == 'number'){
-                let LFGRole = args[1];
-                return;
-            }
-
-            message.member.roles.add(LFGRole);
-
-            break;
-    }});
+}
 
 function sendCardLink(msg, input, availableNames, basePath)
 {
