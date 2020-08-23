02388b199f4ce2f27a8ad3ec98551a359ff3bb6f
SpeedyOlrac <Speedyolrac@yahoo.com>
2020-08-23 17:35:36 -0400

    opps

--- commands/role.js
+++ commands/role.js
@@ -9,7 +9,7 @@ const roleMessage = "Please react to recieve the Following Roles: \
 module.exports = {
 	name: 'role',
 	description: 'adding a role',
-	public: false, //has to be true to show as a command
+	public: true, //has to be true to show as a command
 	execute(msg, args) {
 
         if(msg.author == adminID){
