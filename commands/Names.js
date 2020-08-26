0ac3cb46d16c58b087bd40911678980f76188062
SpeedyOlrac <Speedyolrac@yahoo.com>
2020-08-26 15:38:34 -0400

    Create Names.js

---
+++ commands/Names.js
@@ -0,0 +1,116 @@
+var spirits = [
+    'A Spread of Rampant Green',
+    'Bringer of Dreams and Nightmares',
+    'Downpour Drenches the World',
+    'Finder of Paths Unseen',
+    'Fractured Days Split the Sky',
+    'Grinning Trickster Stirs Up Trouble',
+    'Heart of the Wildfire',
+    'Keeper of the Forbidden Wilds',
+    'Lightning\'s Swift Strike',
+    'Lure of the Deep Wilderness',
+    'Many Minds Move as One',
+    'Ocean\'s Hungry Grasp',
+    'River Surges in Sunlight',
+    'Serpent Slumbering Beneath the Island',
+    'Shadows Flicker Like Flame',
+    'Sharp Fangs Behind the Leaves',
+    'Shifting Memory of Ages',
+    'Shroud of Silent Mist',
+    'Starlight Seeks Its Form',
+    'Stone\'s Unyielding Defiance',
+    'Thunderspeaker',
+    'Vengeance as a Burning Plague',
+    'Vital Strength of the Earth',
+    'Volcano Looming High'
+ ];
+
+ var emote = [
+    '<:SpiritRampant:747558393295929475>',
+    '<:SpiritBodan:747570910751621121>',
+    '<:SpiritDownpour:747558401638268999>',
+    '<:SpiritFinder:747558403810787359>',
+    '<:SpiritFractured:747558406318981191>',
+    '<:SpiritTrickster:747570905709936723>',
+    '<:SpiritWildfire:747558405282988103>',
+    '<:SpiritKeeper:747558404943380491>',
+    '<:SpiritLightning:747558396546252944>',
+    '<:SpiritLure:747558396403646605>',
+    '<:SpiritManyMinds:747558400400818319>',
+    '<:SpiritOceans:747558407292190820>',
+    '<:SpiritRivers:747558402850422804>',
+    '<:SpiritSnek:747558396705767424>',  
+    '<:SpiritShadows:747571324418916393>',
+    '<:SpiritFangs:747570910524997743>',
+    '<:SpiritShifting:747558402959605857>',
+    '<:SpiritShroud:747558393123962957>',
+    '<:SpiritStarlight:747558401424490588>',
+    '<:SpiritStone:747558407200047246>',
+    '<:SpiritThunderspeaker:747558392603738244>',
+    '<:SpiritVengeance:747558391572070520>',
+    '<:SpiritEarth:747558396009512960>',
+    '<:SpiritVolcano:747558401139146834>'   
+ ];
+   //
+
+var adversary = [
+    ['Brandenburg-Prussia', '<:FlagPrussia:742198907332853831>', 1, 2, 4, 6, 7, 9, 10  ],
+    ['England', '<:FlagEngland:742199440126771250>', 1, 3, 4, 6, 7, 9, 10],
+    ['France (Plantation Colony)', '<:FlagRussia:742201076362641408>', 2, 3, 5, 7, 8, 9, 10 ],
+    ['Hapsburg Dynasty', '<:FlagHabsburg:742194656556744814>', 2, 3, 5, 6, 8, 9, 10],
+    ['Russia', '<:FlagFrance:742199211340070955> ' , 1, 3, 4, 6, 7, 9, 11],
+    ['Scotland', '<:FlagScotland:742201184164773891>',1, 4, 6, 7, 8, 10],
+    ['Sweden','<:FlagSweden:742194408325382174>',  1, 2, 3, 5, 6, 7, 8 ]
+
+];
+
+
+
+var panel = [
+    [ "https://imgur.com/nlpGjjH", "https://imgur.com/iK9NTsz"], //Rampant
+    [ "https://imgur.com/UxZyTv2", "https://imgur.com/VUOjO0v"], 
+    [ "https://imgur.com/AJjfgOS", "https://imgur.com/CShexIA"],
+    [ "https://imgur.com/owZAHJY", "https://imgur.com/JtdpxCr"],
+    [ "https://imgur.com/g0S3eN8", "https://imgur.com/7z7V6Ol"],
+    [ "https://imgur.com/XvJYfqH", "https://imgur.com/i6YLeWv"],
+    [ "https://imgur.com/A4FRUV5", "https://imgur.com/Y8lVu5J"],
+    [ "https://imgur.com/PBwq5KE", "https://imgur.com/ZKAblG3"],
+    [ "https://imgur.com/yXE6oHw", "https://imgur.com/WLcU0w9"],
+    [ "https://imgur.com/OmSNTeL", "https://imgur.com/wGPod8z"],
+    [ "https://imgur.com/RZh8Y8g", "https://imgur.com/XIG39qe"],//many minds
+    [ "https://imgur.com/rrLWF5o", "https://imgur.com/UVEIJfA"],
+    [ "https://imgur.com/pmdjfxu", "https://imgur.com/uv0mMRV"],
+    [ "https://imgur.com/PgT7Kyj", "https://imgur.com/cE7YZ3L"],
+    [ "https://imgur.com/FmVImnv", "https://imgur.com/oyk2ZwH"],
+    [ "https://imgur.com/uxw6S5D", "https://imgur.com/IoL5BHz"],
+    [ "https://imgur.com/H1Xrzxw", "https://imgur.com/BB48p9Q"],
+    [ "https://imgur.com/MQDsVg9", "https://imgur.com/sE5Iasm"],
+    [ "https://imgur.com/2UujxIG", "https://imgur.com/pCuo4XJ"],
+    [ "https://imgur.com/9hiaoWd", "https://imgur.com/RqlC8ZP"],
+    [ "https://imgur.com/5xnsvD8", "https://imgur.com/PztVF5L"],
+    [ "https://imgur.com/eZbTaHA", "https://imgur.com/4hnimN9"],
+    [ "https://imgur.com/wgs13CF", "https://imgur.com/5iTl68l"],
+    [ "https://imgur.com/KZwtr5W", "https://imgur.com/XOC05vn"]   //volcano 
+];
+
+var role ="React to this message with the :FlagBlank: emoji to be assigned \
+     the @LFG Role and be notified of games in #looking-for-a-game";
+
+     //  (([^"]*), )
+
+exports.spirits = spirits;
+exports.emote = emote;
+exports.adversary = adversary;
+exports.panel = panel;
+exports.role = role;
+
+//exports.unqiues = unqiues;
+
+
+
+
+
+    
+ 
+
+
