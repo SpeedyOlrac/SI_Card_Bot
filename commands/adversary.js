d0835c129d99762ae31e8206bfb41ffa547de133
SpeedyOlrac <Speedyolrac@yahoo.com>
2020-09-03 16:11:01 -0400

    Update adversary.js

--- commands/adversary.js
+++ commands/adversary.js
@@ -5,18 +5,18 @@ module.exports = {
 	description: 'Get an adversaries',
 	public: true,
 	async execute(msg, args) {
-        console.log(adversary);
+        console.log(adversary.length);
         var panel = "";
-        if (!Array.isArray(args[0]) || args[0].length == 0){
+        if (!Array.isArray(args[0]) || args.length == 0){
              panel = "Adversaries are \n Prussia, Engalnd, France, Habsburg, Russia, Scotland, Sweeden";
         }
         var found = false;
         var list = [];
 
-        for(const n = 0; n < adversary.length; n++ ){
+        for(const ad of adversary){
             //  list.concat(ad.name);
             //cardSearch.getcardname(args[0], list);
-            if(!found && adversary[n].title == args[0].toLowerCase()){
+            if(!found && ad.title == args.toLowerCase()){
                 panel = adversary[n].panel;
                 found = true;
             }
