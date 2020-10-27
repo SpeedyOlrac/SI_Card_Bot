ef78af2885789bee9854a6422203329c7642da4c
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2020-10-27 15:34:35 -0400

    Removing old code

--- commands/spirit.js
+++ commands/spirit.js
@@ -9,33 +9,18 @@ module.exports = {
 	public: true,
 	execute(msg, args) {
         var target = "Sorry could not find the spirit you where looking for.";
-         var availableNames = [];
 
         if(args.length < 1){
             msg.channel.send(target);
             return;
         }
 
+        var availableNames = [];
         for( var s = 0; s < spirits.length; s++){
                 availableNames.push(spirits[s].name);
             }
         var results = getCardName(args, availableNames);
         console.log(results);
-        /* 
-        
-        outer_loop:
-        for(var l = 0; l < args.length; l++){
-            for( var s = 0; s < spirits.length; s++){
-                var name = spirits[s].name.split(" ");
-                for(var n = 0; n < name.length; n++){
-                    if (!found && args[l].length > 3 && name[n].toLowerCase() == args[l].toLowerCase()){                        
-                        target =  s;
-                        found = true;
-                        break ;//outer_loop;
-                    } 
-                }
-            }               
-        } */
 
         //msg.channel.send(spirits[target].title );
         if(result != null){
