7e7f840fa6fdba449441682b1c918f1ff68ea04a
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2020-10-27 14:44:56 -0400

    Update spirit.js

--- commands/spirit.js
+++ commands/spirit.js
@@ -18,6 +18,7 @@ module.exports = {
 
         var name = Array.toString(args).replace(',', " ");
         console.log(name);
+        
         var results = getCardName(name, availableNames);
         /* 
         
