fd750778f8e3bc944b0eff75187b459e4839d51c
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2020-10-28 11:35:26 -0400

    Update power.js

--- commands/power.js
+++ commands/power.js
@@ -9,6 +9,6 @@ module.exports = {
 
 	execute(msg, args) {
 		msg.channel.send(ImageNames.noJE);
-		msg.channel.send(s(msg, args, ImageNames.power, "https://sick.oberien.de/imgs/powers/"));
+		msg.channel.send( s(msg, args, ImageNames.power, "https://sick.oberien.de/imgs/powers/"));
 	},
 };
