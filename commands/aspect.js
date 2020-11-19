c65dbbceaf4ff7aa2ab48f9f35ab88cd0324eb8b
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2020-11-19 17:19:36 -0500

    Update aspect.js

--- commands/aspect.js
+++ commands/aspect.js
@@ -8,7 +8,6 @@ var aspectNames = [ ];
 var target = "Spirts aspects are: \n";
 
 for(var i = 0; i < spiritsNames.length; i++){
-	console.log(spiritsNames[i].aspect);
 	if (spiritsNames[i].aspect != null){
 		spirits.push(spiritsNames[i]);
 	}
@@ -36,7 +35,7 @@ module.exports = {
 	public: true, //has to be true to show as a command
 	execute(msg, args) {
 
-		console.log(spiritsNames);
+		console.log(spirits);
 		console.log(target);		
 		console.log(aspectNames);
 
