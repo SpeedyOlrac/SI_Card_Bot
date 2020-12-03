d4b31ce6a332f457c1660085c6b639ac1e808a8d
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2020-12-03 11:58:59 -0500

    Aspects test

--- commands/aspect.js
+++ commands/aspect.js
@@ -1,65 +1,38 @@
 const { getCardName } = require('./sendCardLink.js');
 
-const spiritsNames = require('./spiritNames.js').spirits;
+const aspectNames = require('./aspectNames.js');
 //const sendCardLink = require("./sendCardLink.js").sendCardLink;
 
 var spirits = [ ];
 var aspectNames = [ ];
 var target = "Spirts aspects are: \n";
 
-for(var i = 0; i < spiritsNames.length; i++){
-	if (spiritsNames[i].aspect.length > 0){
-		spirits.push(spiritsNames[i]);
-	}
-}
-
-
-for(var i = 0; i < spirits.length; i++){
-	target += spirits[i].name + ": ";
-	var stop = 1;
-	console.log(spirits[i].aspect);
-	for (var a = 0; a < spirits[i].aspect.length; a++){
-	
-		target += spirits[i].aspect[a].name;
-		if (stop < spirits[i].aspect[a].length){
-			target += ", ";
-		}
-		else{
-			target += "\n";
-		}
-		stop++;
-		aspectNames.push(spirits[i].aspect[a].name); 
-	}		
-}
 
 module.exports = {
 	name: 'aspect',
 	description: 'The ascpects of sprits',
 	public: true, //has to be true to show as a command
 	execute(msg, args) {
-
-		console.log(spirits);
-		console.log(target);		
-		console.log(aspectNames);
-
-		if(args.length = 0){
-			msg.channel.send(target);
+		if(args.length == 0){
+			args.push("none");
+			args.push("");
 		}
+		else if(args.length == 1){
+			temp = args[0].toLowerCase;
 
-		var found = false;
-		var name = getCardName(args, aspectNames);
-
-		for(var i = 0; i < spirits.length; i++){
-			for(var a = 0; a < spirits[i].length; a++){
-				var aspect = spirits[i].aspect[a];
-				if (!found && aspect.name == name){
-					target = spirits[i].name + ": " + aspect.name + " aspect \n" +
-						aspect.panel; 
-
-					msg.channel.send(target);
-					found = true;
-				}
+			if (aspectNames.aspectsNames.find(temp) != -1){
+				args[0] = "skip";
+				args.push(temp);
+			}
+			else{
+				args.push("none");
 			}
 		}
-	},
+
+		console.log(args[0] + " " args[1]);
+		var message = aspectNames(args[0], args[1]);
+
+		msg.channel.send(message);
+
+	}
 };
--- commands/role.js
+++ commands/role.js
@@ -4,9 +4,9 @@
 const adminID = '176329826641117186';
 const roleMessage = "Welcome to the Spirit Island Discord! \
     \nThe Dahan seek aid! React using <:FlagBlank:742199330018164776> to receive the @LFG role, then post in #looking-for-a-game to find other players. \
-    \nGot questions? #rules-questions  ";
+    \nGot questions? #rules-questions  "; 739893703099023472
 
-const LFG = '747806738308268075'; 
+const LFG = '747806738308268075';  
 
 module.exports = {
 	name: 'role',
