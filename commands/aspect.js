09db4fb4462b51368458de89d961dad1412d7161
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2020-12-04 16:01:10 -0500

    Update aspect.js

--- commands/aspect.js
+++ commands/aspect.js
@@ -7,9 +7,7 @@ module.exports = {
 	public: true, //has to be true to show as a command
 	execute(msg, args) {
 		console.log("aspect command");
-		console.log(aspectsNames);
-		console.log(spirits);
-		console.log(aspects);
+		console.log(aspects[0]);
 
 		var message = "";
 
