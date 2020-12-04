265aab0f35c35af1344cbd7b2513f42817fb907d
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2020-12-04 12:28:02 -0500

    Update of Aspect Names

--- commands/aspect.js
+++ commands/aspect.js
@@ -9,14 +9,14 @@ module.exports = {
 	description: 'The ascpects of sprits',
 	public: true, //has to be true to show as a command
 	execute(msg, args) {
-		
+
 		if(args.length == 0){
 			args.push("none");
 			args.push("");
 		}
 		else if(args.length == 1){
 			temp = args[0].toLowerCase();
-			if (aspectNames.aspectsNames.find(temp) != -1){
+			if (aspectNames.find(temp) != -1){
 				args[0] = "skip";
 				args.push(temp);
 			}
@@ -26,7 +26,6 @@ module.exports = {
 		}
 		
 		console.log(args[0] + " " + args[1]);
-		console.log(aspectsNames);
 		var message = execute(args[0], args[1]);
 
 		msg.channel.send(message);
