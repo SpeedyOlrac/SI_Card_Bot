6558884aabc1e60c9869806bd3a406f1f58f4c7a
Marc-Andre Paris-Cloutier <marc-andrepc@hotmail.com>
2020-12-05 12:28:09 -0500

    Adding perfect match to card selection

--- commands/sendCardLink.js
+++ commands/sendCardLink.js
@@ -1,15 +1,34 @@
 const levenshtein = require('js-levenshtein');
 
-function getCardName(input, availableNames, wieghtOfSizediff = 0.8)
+function getCardName(input, availableNames, weightOfSizediff = 0.8)
+{
+  var target  = cleanInput(input);
+
+  var perfectMatches = getPerfectMatch(target, availableNames);
+
+  if(perfectMatches.length === 1)
+  {
+    return perfectMatches[0];
+  }
+  else if(perfectMatches > 1)
+  {
+    return getCardNameHelper(target, perfectMatches, weightOfSizediff);
+  }
+  else
+  {
+    return getCardNameHelper(target, availableNames, weightOfSizediff);
+  }
+}
+
+function getCardNameHelper(input, availableNames, weightOfSizediff)
 {
   var result = null;
   var closestDistance = 999;
-  var target  = cleanInput(input);
 
   for(var name of availableNames)
   {
-    var sizeDiff = name.length > target.length ? name.length - target.length : 0;
-    var distance = levenshtein(target, name) - (sizeDiff * wieghtOfSizediff);
+    var sizeDiff = name.length > input.length ? name.length - input.length : 0;
+    var distance = levenshtein(input, name) - (sizeDiff * weightOfSizediff);
     if(distance < closestDistance){
       closestDistance = distance;
       result = name;
@@ -18,6 +37,18 @@ function getCardName(input, availableNames, wieghtOfSizediff = 0.8)
   return result;
 }
 
+function getPerfectMatch(input, availableNames)
+{
+  var result = [];
+  for(var name of availableNames)
+  {
+    if(name.includes(input))
+    {
+      result.push(name);
+    }
+  }
+  return result;
+}
 
 async function sendCardLink(msg, input, availableNames, basePath)
 {
@@ -32,7 +63,7 @@ async function sendCardLink(msg, input, availableNames, basePath)
 
 function cleanInput(args){
     var card_name = args.toString().toLowerCase();
-    return card_name.replace("-", "").replace("\'", "").replace(",", "_");
+    return card_name.replace("-", "").replace("\'", "").replace(",", "_").replace(' ', '_');
 }
 
 
