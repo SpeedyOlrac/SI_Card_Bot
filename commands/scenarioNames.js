e9e75fd12635aab291088e40f86c4f3f9cb77cba
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2021-02-11 13:04:09 -0500

    Update scenarioNames.js

--- commands/scenarioNames.js
+++ commands/scenarioNames.js
@@ -1,93 +1,93 @@
 var blitz = {
-    name = "Blitz",
-    front = "",
-    back = "",
-    diffculty = "0"
+    name: "Blitz",
+    front: "",
+    back: "",
+    diffculty: "0"
 }
 
 var GtIH = {
-    name = "Guard the Isle's Heart",
-    front = "",
-    back = "",
-    diffculty = "0"
+    name: "Guard the Isle's Heart",
+    front: "",
+    back: "",
+    diffculty: "0"
 }
 
-var RoT = {
-    name = "Rituals of Terror",
-    front = "",
-    back = "",
-    diffculty = "3"
+var RoT ={
+    name: "Rituals of Terror",
+    front: "",
+    back: "",
+    diffculty: "3"
 }
 
 var DI = {
-    name = "Dahan Insurrection",
-    front = "",
-    back = "",
-    diffculty = "4"
+    name: "Dahan Insurrection",
+    front: "",
+    back: "",
+    diffculty: "4"
 }
 
 var SW = {
-    name = "Second Wave",
-    front = "",
-    back = "",
-    diffculty = "1"
+    name: "Second Wave",
+    front: "",
+    back: "",
+    diffculty: "1"
 }
 
-var PLF = {
-    name = "Powers Long Forgotten",
-    front = "",
-    back = "",
-    diffculty = "1"
+var PLF: = {
+    name: "Powers Long Forgotten",
+    front: "",
+    back: "",
+    diffculty: "1"
 }
 
-var WtS = {
-    name = "Ward the Shores",
-    front = "",
-    back = "",
-    diffculty = "2"
+var WtS: ={
+    name: "Ward the Shores",
+    front: "",
+    back: "",
+    diffculty: "2"
 }
 
 var RotDF = {
-    name = "Rituals of the Destroying Flame",
-    front = "",
-    back = "",
-    diffculty = "3"
+    name: "Rituals of the Destroying Flame",
+    front: "",
+    back: "",
+    diffculty: "3"
 }
 
-var EI = {
-    name = "Elemental Invocation",
-    front = "",
-    back = "",
-    diffculty = "1"
+var EI: = {
+    name: "Elemental Invocation",
+    front: "",
+    back: "",
+    diffculty: "1"
 }
 
 var DT = {
-    name = "Despicable Theft",
-    front = "",
-    back = "",
-    diffculty = "2"
+    name: "Despicable Theft",
+    front: "",
+    back: "",
+    diffculty: "2"
 }
 
 var TGR = {
-    name = "The Great River",
-    front = "",
-    back = "",
-    diffculty = "3"
+    name: "The Great River",
+    front: "",
+    back: "",
+    diffculty: "3"
 }
 
 var ADoS = {
-    name = "A Diversity of Spirits",
-    front = "",
-    back = "",
-    diffculty = "0"
+    name: "A Diversity of Spirits",
+    front: "",
+    back: "",
+    diffculty: "0"
 }
 
 var VT = {
-    name = "Varied Terrains",
-    front = "",
-    back = "",
-    diffculty = "2"
+    name: "Varied Terrains",
+    front: "",
+    back: "",
+    diffculty: "2"
 }
 
-exports.scenario = [blitz, GtIH, RoT, DI, 
+exports.scenario =[blitz, GtIH, RoT, DI, 
   SW, PLF, WtS, RotDF, EI, DT, TGR, ADoS, VT]
