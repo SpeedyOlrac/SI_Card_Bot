93f75ec8aadab09a8a6ac7d34c4e497d65009add
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2021-02-11 12:10:12 -0500

    Update scenario.js

--- commands/scenario.js
+++ commands/scenario.js
@@ -3,11 +3,11 @@
 
 const getCardName = require ('./sendCardLink.js').getCardName;
 const to = require('await-to-js').default;
-const scenario = reuire ( "./scenarioNames.js").scenario;
+const scenario = require ( "./scenarioNames.js").scenario;
 
 module.exports = {
-	name: 'template',
-	description: 'Template',
+	name: 'scenario',
+	description: 'Scenario image caller',
 	public: true, //has to be true to show as a command
 	async execute(msg, args) {
         
@@ -19,7 +19,7 @@ module.exports = {
             names.push(s.name);
         }
 
-        
+
         if (args.length == 0){
             msg.channel.send("Scenarios are: \n" + names);
             return;
