3f1eeb68b05532498f3bff9c967d9216fc8c9b7f
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2021-03-24 20:07:24 -0400

    Update scenarioNames.js

--- commands/scenarioNames.js
+++ commands/scenarioNames.js
@@ -2,91 +2,118 @@ var blitz = {
     name: "Blitz",
     front: "front",
     back: "back",
-    diffculty: "0"
+    diffculty: "0",
+    linkFront: "https://i.imgur.com/l9LsFb7.png" ,
+    linkBack: "https://i.imgur.com/rbm2vox" 
+
 }
 
 var GtIH = {
     name: "Guard the Isle's Heart",
     front: "front",
     back: "back",
-    diffculty: "0"
+    diffculty: "0",
+    link: "https://i/imgur.com/gaQAMPy",
+    linkBack: "https://i/imgur.com/sarg4rv"
 }
 
 var RoT ={
     name: "Rituals of Terror",
     front: "front",
     back: "back",
-    diffculty: "3"
+    diffculty: "3",
+    link: "https://i/imgur.com/ynAA7aW",
+    linkBack: "https://i/imgur.com/NIOiZUI"
 }
 
 var DI = {
     name: "Dahan Insurrection",
     front: "front",
     back: "back",
-    diffculty: "4"
+    diffculty: "4",
+    link: "https://i/imgur.com/gSQuYq7",  
+    linkBack: "https://i/imgur.com/9zzKSqe"
 }
 
 var SW = {
     name: "Second Wave",
     front: "front",
     back: "back",
-    diffculty: "1"
+    diffculty: "1",
+    link: "https://i/imgur.com/ybHof3q",
+    linkBack: "https://i/imgur.com/DkJoMTi"
 }
 
-var PLF = {
+var PLF = {                                             
     name: "Powers Long Forgotten",
     front: "front",
     back: "back",
-    diffculty: "1"
+    diffculty: "1",
+    link: "https://i/imgur.com/XvKFi2N",
+    linkBack: "https://i/imgur.com/KBTIdfs"
 }
 
 var WtS = {
     name: "Ward the Shores",
     front: "front",
     back: "back",
-    diffculty: "2"
+    diffculty: "2",
+    link: "https://i/imgur.com/Y8om7Bb",
+    linkBack: "https://i/imgur.com/5dd2xP0"
 }
 
 var RotDF = {
     name: "Rituals of the Destroying Flame",
     front: "front",
     back: "back",
-    diffculty: "3"
+    diffculty: "3",
+    link: "https://i/imgur.com/S2oxMkF",
+    linkBack: "https://i/imgur.com/L4zMjtC"
 }
 
 var EI = {
     name: "Elemental Invocation",
     front: "front",
     back: "back",
-    diffculty: "1"
+    diffculty: "1",
+    link: "https://i/imgur.com/haOq62U",
+    linkBack: "https://i/imgur.com/4wTQqjQ"
 }
 
 var DT = {
     name: "Despicable Theft",
     front: "front",
     back: "back",
-    diffculty: "2"
+    diffculty: "2",
+    link: "https://i/imgur.com/RTEQFIQ",
+    linkBack: "https://i/imgur.com/VLyEXFg"
 }
 
 var TGR = {
     name: "The Great River",
     front: "front",
     back: "back",
-    diffculty: "3"
+    diffculty: "3",
+    link: "https://i/imgur.com/xEgVnVE",
+    linkBack: "https://i/imgur.com/R9oOeXL"
 }
 
 var ADoS = {
     name: "A Diversity of Spirits",
     front: "front",
     back: "back",
-    diffculty: "0"
+    diffculty: "0",
+    link: "https://i/imgur.com/24wwuun",
+    linkBack: "https://i/imgur.com/iF15h8W"
 }
 
 var VT = {
     name: "Varied Terrains",
     front: "front",
     back: "back",
-    diffculty: "2"
+    diffculty: "2",
+    link: "https://i/imgur.com/XbGRf3Z",
+    linkBack: "https://i/imgur.com/rOg2M2t"
 }
 
 exports.scenario =[blitz, GtIH, RoT, DI, 
