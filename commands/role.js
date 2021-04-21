e9685843f85395b4cec1f10362b197bea2eccfdd
SpeedyOlrac <47254122+SpeedyOlrac@users.noreply.github.com>
2021-04-21 15:13:21 -0400

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -23,9 +23,10 @@ module.exports = {
         const lfgChannel = "<#739893703099023472>";
         const ruleChannel = "<#693569012075855872>"
 
-        const LFGEmote = 'FlagBlank';
-        const PBPEmote = '5SpeedSlow';
-        const AmoungUsEmote = '0AmongUs';
+        const LFGEmote = message.guild.emojis.cache.find(emoji => emoji.name === 'FlagBlank');
+        const PBPEmote = message.guild.emojis.cache.find(emoji => emoji.name === '5SpeedSlow');
+        const AmoungUsEmote = message.guild.emojis.cache.find(emoji => emoji.name === '0AmongUs');
+        
         
        // message.delete();
 
@@ -35,10 +36,10 @@ module.exports = {
             .setDescription("The Dahan seek aid!")
             .setThumbnail('https://i.imgur.com/QbkXAHr.jpg?1')
             .addFields(
-                { name: 'Looking for Group', value: " React with <:FlagBlank:834496282734624828>" +" to receive the @LFG role, then post in " + 
+                { name: 'Looking for Group', value: " React with <:FlagBlank:>" +" to receive the @LFG role, then post in " + 
                 lfgChannel + " to find other players." },
-                { name: 'Play by Post', value: "React with <:5SpeedSlow:834496282734624828>" + " to join the play by post section." },
-                { name: 'Among Us', value: "React with <:0AmongUs:834496282734624828>" + " to join the Among Us group." },
+                { name: 'Play by Post', value: "React with <:5SpeedSlow:>" + " to join the play by post section." },
+                { name: 'Among Us', value: "React with <:0AmongUs:>" + " to join the Among Us group." },
                 { name: 'Rules Question', value: "Visit the rules Channel " + ruleChannel }
             );
  
