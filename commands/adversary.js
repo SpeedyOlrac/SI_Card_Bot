104a472becf5e0be36f81b7487594e1aeb8eeaa1
thirdratecyberpunk <blackbul@aston.ac.uk>
2023-08-29 12:32:32 +0100

    Adversary command now lists valid aliases

--- commands/adversary.js
+++ commands/adversary.js
@@ -4,29 +4,40 @@ const getcardname = require('./sendCardLink.js').getCardName;
 
 module.exports = {
 	name: 'adversary',
-	description: 'Get an adversary',
+	description: 'Get a single adversary panel',
 	public: true,
 	async execute(msg, args) {          
-        var panel =  "Adversaries are \nPrussia, England, France, Habsburg (Livestock-Colony, HLC, Cowburg), Habsburg (Mining-Expedition, HME, Saltburg), Russia, Scotland, Sweden";
+        var panel =  "";
         var found = false;
         var list = [];
         
+        // if an adversary parameter is provided
         if (args.length != 0){
            const searchString = args[0].toLowerCase();
            for(const adversary of ad.adversaries){
-                console.log(adversary);
                 // direct match
                 if(adversary.name.toLowerCase().indexOf(searchString) >= 0) {
                     panel = adversary.panel;
+                    found = true;
+                    break;
                 }
                 // alias
                 else{
                     for (const alias of adversary.alias){
                         if (alias.toLowerCase().indexOf(searchString) >= 0){
                             panel = adversary.panel;
+                            found = true;
+                            break;
                         }
                     }
                 }
+            }
+        }
+        // if no match found or no argument provided, assume they want a list of adversaries
+        if (args.length == 0 | !found){
+            panel = "Choose an adversary: (aliases) \n";
+            for (const adversary of ad.adversaries){
+                panel += "* " + adversary.name + " (" + adversary.alias.join(" , ") + ")\n";
             }
         }
 
