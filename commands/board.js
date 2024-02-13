7f0b2e4c19e0bd91eae17180538872dc66d70082
thirdratecyberpunk <blackbul@aston.ac.uk>
2024-02-13 22:57:39 +0000

    Now sanitises board search input and updated help string

--- commands/board.js
+++ commands/board.js
@@ -1,7 +1,7 @@
 // can be used as template
 //save as command as commandName.js
 
-
+const helpString = "Type the board name. -board [a, b, c, d, e, f, g, h, NE/northeast, NW/northwest, East, West/W, SE/southeast, SW/southwest]."
 
 module.exports = {
 	name: 'board',
@@ -10,11 +10,9 @@ module.exports = {
 	async execute(msg, args) {
 
         if(args[0] ==  "help" || args[0] == "" ){
-
-            await msg.channel.send("Type the board name. -board [a,b,c,d,e,f,NE, NW, East, West, SE,SW].");
+            await msg.channel.send(helpString);
         }
         else{
-
             await msg.channel.send(searchBoards(args[0]));
         }
 
@@ -24,75 +22,64 @@ module.exports = {
 
 function searchBoards(b)
 {
-    switch (b){
+
+    if(b){
+        searchTerm = b.toLowerCase();
+    }
+    else{
+        return helpString;
+    }
+    console.log(b.toLowerCase());
+    switch (searchTerm){
         case "a":
-        case "A":
             return "https://spiritislandwiki.com/images/e/e2/Piece_core_board_a.png";
     break;
-    
         case "b":
-        case "B":
             return "https://spiritislandwiki.com/images/7/7a/Piece_core_board_b.png";
     break;
-    
         case "c":
-        case "C":
             return "https://spiritislandwiki.com/images/8/82/Piece_core_board_c.png";
     break;
-    
         case "d":
-        case "D":
             return "https://spiritislandwiki.com/images/3/3a/Piece_core_board_d.png";
     break;
-    
         case "e":
-        case "E":
             return "https://spiritislandwiki.com/images/e/e5/Piece_je_board_e.png";
     break;
-    
         case "f":
-        case "F":
             return "https://spiritislandwiki.com/images/0/0b/Piece_je_board_f.png";
     break;
-
         case "g":
-        case "G":
             return "https://spiritislandwiki.com/images/d/d4/Piece_horizons_board_g.png";
-
     break;
         case "h":
-        case "H":
             return "https://spiritislandwiki.com/images/4/4a/Piece_horizons_board_h.png";
     
     //Thematic boards
-        case "NE":
         case "ne":
+        case "northeast":
             return "https://spiritislandwiki.com/images/f/f9/Piece_core_board_north_east.png";
     break;
-        case "NW":
         case "nw":
+        case "northwest":
             return "https://spiritislandwiki.com/index.php?title=File:Piece_core_board_east.png";
     break;
-    
-        case "East":
         case "east":
             return "https://spiritislandwiki.com/images/5/58/Piece_core_board_east.png";
     break;
-    
         case "west":
-        case "West":
+        case "w":
             return "https://spiritislandwiki.com/images/2/26/Piece_core_board_west.png";
     break;
-    
-        case "SE":
         case "se":
+        case "southeast":
             return "https://spiritislandwiki.com/images/c/cc/Piece_je_board_south_east.png";
     break;
-        case "SW":
         case "sw":
+        case "southwest":
             return "https://spiritislandwiki.com/images/6/64/Piece_je_board_south_west.png";
     break;
     default:
-       return "Type the board name. -board [a, b, c, d, e, f, g, h, NE, NW, East, West, SE, SW]."; 
+       return helpString; 
     }
 }
