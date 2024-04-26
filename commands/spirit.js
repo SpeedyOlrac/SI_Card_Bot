d60b361663e22113613506522e57b01cb48a14c2
thirdratecyberpunk <blackbul@aston.ac.uk>
2024-04-26 17:58:23 +0100

    Migrated spirit search logic into reusable method

--- commands/spirit.js
+++ commands/spirit.js
@@ -8,98 +8,111 @@ module.exports = {
 	description: 'Spirit Search',
 	public: true,
 	async execute(msg, args) {
-        if (args.length < 1) {
-            return await msg.channel.send("Please provide at least 3 letters to query with.");
-        }
-
-        // Handle result-modifying args
-        const mods = {
-            'front': false,
-            'back': false,
-            'unique': false
-        }
-
-        for (arg in mods) {
-            const idx = args.indexOf(arg);
-            if (idx >= 0) {
-                mods[arg] = true;
-                args.splice(idx, 1);
+        try{
+            if (args.length < 1) {
+                return await msg.channel.send("Please provide at least 3 letters to query with.");
             }
-        }
-        const searchString = args.join(" ").toLowerCase();
+    
+            // Handle result-modifying args
+            const mods = {
+                'front': false,
+                'back': false,
+                'unique': false
+            }
+    
+            for (arg in mods) {
+                const idx = args.indexOf(arg);
+                if (idx >= 0) {
+                    mods[arg] = true;
+                    args.splice(idx, 1);
+                }
+            }
 
-        // stops people searching for small strings that could return all the spirits
-        // TODO: get this safety check to work while not breaking emoji name support
-        // if (searchString.length < 3){
-        //     return await msg.channel.send("Please provide at least 3 letters to query with.");
-        // }
-
-        let foundSpirits = [];
-        
-        // Start with simple substring search
-        for (let i=0; i < spirits.length; i++) {
-            const spirit = spirits[i];
-            if (spirit.name.toLowerCase().indexOf(searchString) >= 0) {
-                foundSpirits.push(spirit);
+            const possibleSpirits = searchForSpirit(args.join(" ").toLowerCase());
+    
+            // if levenshtein returns a single spirit, return that
+            if (possibleSpirits.length === 1) {
+                return await sendSpirit(mods, msg, possibleSpirits[0]);
             }
             else {
-                for (alias of spirit.aliases) {
-                    if (alias.indexOf(searchString) >= 0) {
-                        foundSpirits.push(spirit);
+                    // if they somehow have multiple matches default to the choices option
+                    globals.choices = foundSpirits.map((spirit) => {
+                        return {
+                            "label": `${spirit.emote} ${spirit.name}`,
+                            "value": mods.back ? spirit.panel[1]: spirit.panel[0]
+                        }
+                    });
+                    let message = "Multiple matching spirits found. Select one with _-choose <num>_ (e.g. _-choose 2_)\n"
+                    for (choiceIdx in globals.choices) {
+                        let choice = globals.choices[choiceIdx];
+                        message += `\n${parseInt(choiceIdx) + 1}) ${choice.label}`
                     }
+                    return await msg.channel.send(message);
                 }
-            }
         }
+        catch (e){
+            console.log(e);
+            return msg.channel.send(e.toString());
+        }
+    }
+}
 
-        let smallestDistance = Infinity;
-        // If not found, find by levenshtein distance on all available substrings
-        if (foundSpirits.length === 0) {
-            for (spirit of spirits) {
-                const spiritName = spirit.name.toLowerCase();
-                for (let i=0; i <= spiritName.length - searchString.length; i++) {
-                    const subString = spiritName.substring(i, i+searchString.length);
-                    const distance = levenshtein(subString, searchString);
-                    if (distance < smallestDistance) {
-                        smallestDistance = distance;
-                        foundSpirits = [spirit];
-                    }
-                }
-            }
-        }
+/**
+ * Returns a spirit object when given a string
+ * @param {*} spiritName 
+ */
+function searchForSpirit(searchStringParam){
+    if (searchStringParam.length < 3) {
+        throw new Error("Please provide at least 3 letters to query with.");
+    }
+    const searchString = searchStringParam.toLowerCase();
 
-        // if levenshtein returns a single spirit, return that
-        if (foundSpirits.length === 1) {
-            return await sendSpirit(mods, msg, foundSpirits[0]);
-        }
-        else if (foundSpirits.length > 1) {
-            // first, try to filter the ones found to spirits which have that EXACT word (ignoring apostrophised plurals for Stone's), not just a substring match
-            uniqueSpirits = foundSpirits.filter((foundSpirit)=>
-                sanitiseSpiritName(foundSpirit.name).indexOf(searchString) > -1
-            );
-            console.log(uniqueSpirits);
-            // if there's just one, return that
-            if (uniqueSpirits.length == 1){
-                return await sendSpirit(mods, msg, uniqueSpirits[0]);
-            }
-            else{
-                // if they somehow have multiple matches default to the choices option
-                globals.choices = foundSpirits.map((spirit) => {
-                    return {
-                        "label": `${spirit.emote} ${spirit.name}`,
-                        "value": mods.back ? spirit.panel[1]: spirit.panel[0]
-                    }
-                });
-                let message = "Multiple matching spirits found. Select one with _-choose <num>_ (e.g. _-choose 2_)\n"
-                for (choiceIdx in globals.choices) {
-                    let choice = globals.choices[choiceIdx];
-                    message += `\n${parseInt(choiceIdx) + 1}) ${choice.label}`
-                }
-                return await msg.channel.send(message);
-            }
+    let foundSpirits = [];
+    
+    // Start with simple substring search
+    for (let i=0; i < spirits.length; i++) {
+        const spirit = spirits[i];
+        if (spirit.name.toLowerCase().indexOf(searchString) >= 0) {
+            foundSpirits.push(spirit);
         }
         else {
-            return await msg.channel.send("Sorry, could not find the spirit you were looking for.")
+            for (alias of spirit.aliases) {
+                if (alias.indexOf(searchString) >= 0) {
+                    foundSpirits.push(spirit);
+                }
+            }
         }
+    }
+
+    let smallestDistance = Infinity;
+    // If not found, find by levenshtein distance on all available substrings
+    if (foundSpirits.length === 0) {
+        for (spirit of spirits) {
+            const spiritName = spirit.name.toLowerCase();
+            for (let i=0; i <= spiritName.length - searchString.length; i++) {
+                const subString = spiritName.substring(i, i+searchString.length);
+                const distance = levenshtein(subString, searchString);
+                if (distance < smallestDistance) {
+                    smallestDistance = distance;
+                    foundSpirits = [spirit];
+                }
+            }
+        }
+    }
+
+    // if levenshtein returns a single spirit, return that
+    if (foundSpirits.length === 1) {
+        return foundSpirits;
+    }
+    else if (foundSpirits.length > 1) {
+        // try to filter the ones found to spirits which have that EXACT word (ignoring apostrophised plurals for Stone's), not just a substring match
+        uniqueSpirits = foundSpirits.filter((foundSpirit)=>
+            sanitiseSpiritName(foundSpirit.name).indexOf(searchString) > -1
+        );
+        return uniqueSpirits;
+    }
+    else {
+        throw new Error('Search string resulted in zero matches. Please try another search term (for example, Serpent or River).');
     }
 }
 
@@ -110,7 +123,7 @@ module.exports = {
 function sanitiseSpiritName(spiritName){
     let spiritWords = spiritName.split(' ');
     let x = spiritWords.map((word)=>{
-        return word.replace("'s", 's').toLowerCase()
+        return word.replace("'s", "s").toLowerCase()
     });
     return x;
 }
